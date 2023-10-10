8552f1433d4d6258eb83be2d583bcdb478e6b88e
Toby Cabot <toby@acnodal.io>
2023-10-10 16:25:30 -0400

    Improvements to brmgr.sh suggested by shellcheck
    Signed-off-by: Toby Cabot <toby@acnodal.io>

--- scripts/brmgr.sh
+++ scripts/brmgr.sh
@@ -3,29 +3,26 @@
 brname=$USER-epic0
 
 if [ "X$1" = "X" ]; then
-    echo $0
+    echo "$0"
     echo "Needs a parameter: either \"up\" or \"destroy\""
     exit 1
 fi
 
-if [ $1 = "up" ]; then
+if [ "$1" = "up" ]; then
 
-    brctl show $brname 2> /dev/null
+    brctl show "$brname" 2> /dev/null
 
     if [ $? = 1 ]; then
         echo "creating $brname"
-        sudo brctl addbr $brname
-        sudo ip link set $brname up
+        sudo brctl addbr "$brname"
+        sudo ip link set "$brname" up
     fi
 fi
 
-if [ $1 = "destroy" ]; then
-
-    brctl show $brname > /dev/null 2>&1
-
-    if [ $? = 0 ]; then
+if [ "$1" = "destroy" ]; then
+    if brctl show "$brname" > /dev/null 2>&1; then
         echo "destroying $brname"
-        sudo ip link set $brname down
-        sudo brctl delbr $brname
+        sudo ip link set "$brname" down
+        sudo brctl delbr "$brname"
     fi
 fi
